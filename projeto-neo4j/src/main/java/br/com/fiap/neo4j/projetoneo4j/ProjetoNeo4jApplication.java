ed56e72b6b280c09d683302c04ce868c4981630f
luizkt <keese.luiz@gmail.com>
2019-06-29 16:22:38 -0300

    Primeiro commit com os projetos neo4j e jpa

---
+++ projeto-neo4j/src/main/java/br/com/fiap/neo4j/projetoneo4j/ProjetoNeo4jApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.neo4j.projetoneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNeo4jApplication.class, args);
+	}
+
+}
---
+++ projeto-neo4j/src/test/java/br/com/fiap/neo4j/projetoneo4j/ProjetoNeo4jApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.fiap.neo4j.projetoneo4j;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoNeo4jApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
