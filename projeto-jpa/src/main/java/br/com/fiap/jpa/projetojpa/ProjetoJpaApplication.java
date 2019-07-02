0ac68ad8dd5c82f9888596318c580837adb68ada
luizkt <keese.luiz@gmail.com>
2019-07-01 22:39:05 -0300

    Ajuste de configuração do banco mysql

--- projeto-jpa/src/main/java/br/com/fiap/jpa/projetojpa/ProjetoJpaApplication.java
+++ projeto-jpa/src/main/java/br/com/fiap/jpa/projetojpa/ProjetoJpaApplication.java
@@ -2,8 +2,13 @@ package br.com.fiap.jpa.projetojpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ComponentScan("br.com.fiap")
+//@ContextConfiguration("resources/")
+@ImportResource({"classpath*:spring.xml"})
 public class ProjetoJpaApplication {
 
 	public static void main(String[] args) {
--- projeto-jpa/src/test/java/br/com/fiap/jpa/projetojpa/ProjetoJpaApplicationTests.java
+++ projeto-jpa/src/test/java/br/com/fiap/jpa/projetojpa/ProjetoJpaApplicationTests.java
@@ -3,6 +3,7 @@ package br.com.fiap.jpa.projetojpa;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +12,7 @@ public class ProjetoJpaApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		 
 	}
-
+	
 }
