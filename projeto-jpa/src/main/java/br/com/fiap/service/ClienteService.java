fbf25b73d137d6e4211439fc087e7c8a6959f907
jhonatan <jhonatan6146@gmail.com>
2019-07-02 19:23:56 -0300

    alterados services de Cliente e Pedido

--- projeto-jpa/src/main/java/br/com/fiap/service/ClienteService.java
+++ projeto-jpa/src/main/java/br/com/fiap/service/ClienteService.java
@@ -1,5 +1,38 @@
 package br.com.fiap.service;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.fiap.entity.Cliente;
+import br.com.fiap.repository.ClienteRepository;
+
 public class ClienteService {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Transactional
+	public void add(Cliente cliente) {
+		clienteRepository.save(cliente);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+
+	@Transactional
+	public void addAll(Collection<Cliente> clientes) {
+		for (Cliente cliente : clientes) {
+			clienteRepository.save(cliente);
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public List<Cliente> findByName(String nome) {
+		return clienteRepository.findByName(nome);
+	}	
 }
--- projeto-jpa/src/main/java/br/com/fiap/service/PedidoService.java
+++ projeto-jpa/src/main/java/br/com/fiap/service/PedidoService.java
@@ -1,5 +1,39 @@
 package br.com.fiap.service;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.fiap.entity.Pedido;
+import br.com.fiap.repository.PedidoRepository;
+
 public class PedidoService {
 
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Transactional
+	public void add(Pedido pedido) {
+		pedidoRepository.save(pedido);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Pedido> findAll() {
+		return pedidoRepository.findAll();
+	}
+
+	@Transactional
+	public void addAll(Collection<Pedido> pedidos) {
+		for (Pedido pedido : pedidos) {
+			pedidoRepository.save(pedido);
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public List<Pedido> findByName(String nome) {
+		return pedidoRepository.findByName(nome);
+	}
+	
 }
